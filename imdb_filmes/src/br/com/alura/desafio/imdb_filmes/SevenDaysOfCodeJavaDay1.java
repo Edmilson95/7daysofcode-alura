f1eb9fe28ad7c637ae60e32d60a048822771c4fb
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-02-25 15:10:07 -0300

    desafio 02 do 7daysofcode alura

--- imdb_filmes/src/br/com/alura/desafio/imdb_filmes/SevenDaysOfCodeJavaDay1.java
+++ imdb_filmes/src/br/com/alura/desafio/imdb_filmes/SevenDaysOfCodeJavaDay1.java
@@ -7,18 +7,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class CriandoRequisicao {
-	
+public class SevenDaysOfCodeJavaDay1 {
 	
 	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 		// cliente HTTP
 		HttpClient client = HttpClient.newHttpClient();
 		
+		String apiKey = "k_x062wd9t";
+		
 		// criar a requisição
 		HttpRequest request = HttpRequest
 					.newBuilder()
 					.GET()
-					.uri(new URI("https://imdb-api.com/en/API/Top250Movies/k_x062wd9t"))
+					.uri(new URI("https://imdb-api.com/en/API/Top250TVs/" + apiKey ))
 					.build();
 		
 		// enviando uma solicitação
@@ -29,7 +30,23 @@ public class CriandoRequisicao {
 		// imprimir o conteudo recebido 
 //		System.out.println(response.statusCode());
 		System.out.println(json);
+//		String[] array = json.split("},");
+//		System.out.println(array[0]);
+//		
+		
+//		String titles[] = json.split("title", 0);
+//		System.out.println(titles[1]);
+//		
+//		String Urls[] = json.split("https:");
+//		System.out.println(Urls[1]);
+//		
+//		String years[] = json.split("year");
+//		System.out.println(years[1]);
+//		
+//		String notas[] = json.split("imDbRating");
+//		System.out.println(notas[1]);
 		
 		
 	}
 }
+
